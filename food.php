af9eb74df235364db75ebc46a91c06491cc62f9e
mjseok <mjseok98@gmail.com>
2020-07-02 17:58:49 +0900

    minji

--- food.php
+++ food.php
@@ -1,5 +1,6 @@
 <?
 include 'topnav.html';
+include './map.html';
 ?>
 <link href="https://fonts.googleapis.com/css?family=Nanum+Brush+Script" rel="stylesheet">
 <center>
@@ -7,14 +8,5 @@ include 'topnav.html';
 <h1> <span style="font-size: 35pt;"><span style="font-family: 'Nanum Brush Script',cursive;">학교 근처 맛집</span></h1>
 <br>
 <?
-  include 'map.html';
+
 ?>
-<style>
-#info {
-  position: absolute;
-  top: 160px;
-  right: 16px;
-  height: 40%;
-  width: 50%;
-}
-</style>
